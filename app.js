9ff57318b7eaa76e41389832ebb0f5b2821373ff
Balin Warren <balin.warren@gmail.com>
2023-04-12 14:29:40 -0700

    Added gameTracker module to place marks on gameboard

--- app.js
+++ app.js
@@ -5,14 +5,45 @@ const gameBoard = (() => {
         for (let i = 0; i < 9; i++) {
             let num = i.toString();
             let square = document.getElementById(num);
-            squareArr.push(square);
+            squareArr.push(document.getElementById(num));
+            square.onclick = () => gameTracker.addMarks(square.id);
         }
         console.log(squareArr);
     }
-    
+
     return {
         buildBoard
     };
 })();
 
+const gameTracker = (() => {
+    let turnCount = 1;
+
+    const addMarks = (index) => {
+        let square = document.getElementById(index);
+
+        if(turnCount % 2 == 0){
+            let mark = document.createElement('h3');
+            mark.classList.add('o');
+            mark.innerHTML = 'O';
+            square.appendChild(mark);
+        } else {
+            let mark = document.createElement('h3');
+            mark.classList.add('x');
+            mark.innerHTML = 'X';
+            square.appendChild(mark);
+        }
+
+        doTurn(turnCount);
+    }
+
+    const doTurn = () => {
+        turnCount++;
+        console.log(turnCount);
+    }
+
+    return {
+        addMarks
+    }
+})();
 gameBoard.buildBoard();
