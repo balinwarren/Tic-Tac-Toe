74314d47a50ceb35e30e14b6871c227c3883e30b
Balin Warren <balin.warren@gmail.com>
2023-04-12 15:37:11 -0700

    Added check win function

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const gameBoard = (() => {
     }
 
     return {
-        buildBoard
+        buildBoard,
+        squareArr
     };
 })();
 
@@ -34,16 +35,55 @@ const gameTracker = (() => {
             square.appendChild(mark);
         }
 
-        doTurn(turnCount);
+        doTurn();
     }
 
     const doTurn = () => {
+        if(turnCount >= 6) {
+            checkWin();
+        }
+
         turnCount++;
         console.log(turnCount);
     }
 
+    const checkWin = () => {
+        let marksArr = [];
+        for(let i = 0; i < 9; i++) {
+            let square = gameBoard.squareArr[i];
+            let squareMark = square.firstElementChild;
+
+            if (squareMark == null){
+                marksArr.push('null');
+            } else {
+                marksArr.push(squareMark.innerHTML);
+            }
+        }
+
+        if (marksArr[0] == marksArr[1] && marksArr[1] == marksArr[2]) {
+            console.log(`The winner is ${marksArr[0]}`);
+        } else if (marksArr[3] == marksArr[4] && marksArr[4] == marksArr[5]) {
+            console.log(`The winner is ${marksArr[3]}`);
+        } else if (marksArr[6] == marksArr[7] && marksArr[7] == marksArr[8]) {
+            console.log(`The winner is ${marksArr[6]}`);
+        } else if (marksArr[0] == marksArr[3] && marksArr[3] == marksArr[6]) {
+            console.log(`The winner is ${marksArr[0]}`);
+        } else if (marksArr[1] == marksArr[4] && marksArr[4] == marksArr[7]) {
+            console.log(`The winner is ${marksArr[1]}`);
+        } else if (marksArr[2] == marksArr[5] && marksArr[5] == marksArr[8]) {
+            console.log(`The winner is ${marksArr[2]}`);
+        } else if (marksArr[0] == marksArr[4] && marksArr[4] == marksArr[8]) {
+            console.log(`The winner is ${marksArr[0]}`);
+        } else if (marksArr[2] == marksArr[4] && marksArr[4] == marksArr[6]) {
+            console.log(`The winner is ${marksArr[2]}`);
+        } else if (turnCount == 9) {
+            console.log("It's a tie!")
+        }
+    }
+
     return {
-        addMarks
+        addMarks,
     }
 })();
+
 gameBoard.buildBoard();
